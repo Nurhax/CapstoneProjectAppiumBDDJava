40172de9ce2d353d1b5e6c627762a84c37e861da
Nurhax <iqbalnur2009@gmail.com>
2026-05-01 16:33:10 +0700

    remove unused lib and add new feature file for customer role

--- src/test/java/com/TA/steps/AuthSteps.java
+++ src/test/java/com/TA/steps/AuthSteps.java
@@ -21,7 +21,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.Random;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 /**
  *

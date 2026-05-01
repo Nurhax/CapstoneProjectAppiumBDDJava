cc27879e481f15eb83ab44261916ce69f7b31059
Nurhax <iqbalnur2009@gmail.com>
2026-05-01 17:01:15 +0700

    restrukturasi dan menambahkan preparasi untuk e2e customer

--- src/test/java/com/TA/steps/Auth/AuthSteps.java
+++ src/test/java/com/TA/steps/Auth/AuthSteps.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.TA.steps;
+package com.TA.steps.Auth;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
---
+++ src/test/java/com/TA/steps/Customer/TransaksiBookingSteps.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.TA.steps.Customer;
+
+/**
+ *
+ * @author KnightlyTech
+ */
+public class TransaksiBookingSteps {
+    
+}
